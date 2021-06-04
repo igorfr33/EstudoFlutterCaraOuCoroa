aa988c24808d82fd692449e1110659e7d84ffd86
Igor Rabelo <igorrabelof10@gmail.com>
2021-06-04 17:16:14 -0300

    App de Estudo Cara ou Coroa

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cara_ou_coroa/telas/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
